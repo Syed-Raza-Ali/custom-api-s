f282809a7e73eb39b5eb5cbed3827b73bc9039da
Syed Raza Ali <itsrazaalishah303@gmail.com>
2024-01-15 14:07:58 -0700

    some

--- src/app/route.ts
+++ src/app/route.ts
@@ -30,25 +30,8 @@ export async function GET(request: NextRequest) {
           description: "Sweatshirt",
           price: "500$",
           image: "https://i.ibb.co/WsWwgPG/lady05.png"
-        },
-        { imageID: "3",
-          title: "Flex Sweatshirt",
-          description: "Sweatshirt",
-          price: "500$",
-          image: "https://i.ibb.co/LtWZ5c7/lady03.png"
-        },
-        { imageID: "4",
-          title: "Flex Sweatshirt",
-          description: "Sweatshirt",
-          price: "500$",
-          image: "https://i.ibb.co/qjZ2Pgc/lady04.png"
-        },
-        { imageID: "5",
-          title: "Flex Sweatshirt",
-          description: "Sweatshirt",
-          price: "500$",
-          image: "https://i.ibb.co/WsWwgPG/lady05.png"
         }
+        
       ]
-    return NextResponse.json( {data : jobs} )
+    return NextResponse.json(  jobs  )
 }
