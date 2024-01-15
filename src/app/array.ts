d48ff91dfa2ccf17d47f06cd065a81f83b9abcdd
Syed Raza Ali <itsrazaalishah303@gmail.com>
2024-01-15 12:30:46 -0700

    For api's fetching

---
+++ src/app/array.ts
@@ -0,0 +1,122 @@
+export const PromotionImages = [
+  { imageID: "1",
+    title: "Flex Sweatshirt",
+    description: "Sweatshirt",
+    price: "500$",
+    image: "https://i.ibb.co/SfLHCs7/lady01.png"
+  },
+  { imageID: "2",
+    title: "Flex Sweatshirt",
+    description: "Sweatshirt",
+    price: "500$",
+    image: "https://i.ibb.co/2WVC4MV/lady02.png"
+  },
+  { imageID: "3",
+    title: "Flex Sweatshirt",
+    description: "Sweatshirt",
+    price: "500$",
+    image: "https://i.ibb.co/LtWZ5c7/lady03.png"
+  },
+  { imageID: "4",
+    title: "Flex Sweatshirt",
+    description: "Sweatshirt",
+    price: "500$",
+    image: "https://i.ibb.co/qjZ2Pgc/lady04.png"
+  },
+  { imageID: "5",
+    title: "Flex Sweatshirt",
+    description: "Sweatshirt",
+    price: "500$",
+    image: "https://i.ibb.co/WsWwgPG/lady05.png"
+  },
+  { imageID: "3",
+    title: "Flex Sweatshirt",
+    description: "Sweatshirt",
+    price: "500$",
+    image: "https://i.ibb.co/LtWZ5c7/lady03.png"
+  },
+  { imageID: "4",
+    title: "Flex Sweatshirt",
+    description: "Sweatshirt",
+    price: "500$",
+    image: "https://i.ibb.co/qjZ2Pgc/lady04.png"
+  },
+  { imageID: "5",
+    title: "Flex Sweatshirt",
+    description: "Sweatshirt",
+    price: "500$",
+    image: "https://i.ibb.co/WsWwgPG/lady05.png"
+  }
+]
+  
+
+  export const mainImages = [
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/SfLHCs7/lady01.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/LtWZ5c7/lady03.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/qjZ2Pgc/lady04.png"
+    },
+  ]
+  export const femaleImages = [
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/SfLHCs7/lady01.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/2WVC4MV/lady02.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/LtWZ5c7/lady03.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/qjZ2Pgc/lady04.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/WsWwgPG/lady05.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/LtWZ5c7/lady03.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/qjZ2Pgc/lady04.png"
+    },
+    {
+      title: "Flex Sweatshirt",
+      description: "Sweatshirt",
+      price: "500$",
+      image: "https://i.ibb.co/WsWwgPG/lady05.png"
+    }
+  ]
---
+++ src/app/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from "next/server";
+export async function GET(request: NextRequest) {
+    let jobs = [
+        { imageID: "1",
+          title: "Flex Sweatshirt",
+          description: "Sweatshirt",
+          price: "500$",
+          image: "https://i.ibb.co/SfLHCs7/lady01.png"
+        },
+        { imageID: "2",
+          title: "Flex Sweatshirt",
+          description: "Sweatshirt",
+          price: "500$",
+          image: "https://i.ibb.co/2WVC4MV/lady02.png"
+        },
+        { imageID: "3",
+          title: "Flex Sweatshirt",
+          description: "Sweatshirt",
+          price: "500$",
+          image: "https://i.ibb.co/LtWZ5c7/lady03.png"
+        },
+        { imageID: "4",
+          title: "Flex Sweatshirt",
+          description: "Sweatshirt",
+          price: "500$",
+          image: "https://i.ibb.co/qjZ2Pgc/lady04.png"
+        },
+        { imageID: "5",
+          title: "Flex Sweatshirt",
+          description: "Sweatshirt",
+          price: "500$",
+          image: "https://i.ibb.co/WsWwgPG/lady05.png"
+        },
+        { imageID: "3",
+          title: "Flex Sweatshirt",
+          description: "Sweatshirt",
+          price: "500$",
+          image: "https://i.ibb.co/LtWZ5c7/lady03.png"
+        },
+        { imageID: "4",
+          title: "Flex Sweatshirt",
+          description: "Sweatshirt",
+          price: "500$",
+          image: "https://i.ibb.co/qjZ2Pgc/lady04.png"
+        },
+        { imageID: "5",
+          title: "Flex Sweatshirt",
+          description: "Sweatshirt",
+          price: "500$",
+          image: "https://i.ibb.co/WsWwgPG/lady05.png"
+        }
+      ]
+    return NextResponse.json( {data : jobs} )
+}
